60487ef21a4f64a95335455499ee0b22b90ba560
Vivek-baxla <144560722+Vivek-baxla@users.noreply.github.com>
2023-09-22 18:00:36 +0530

    solved assignments files are added

---
+++ predictOutput1.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cout<<"Enter first number : "<<endl;
+    cin>>x;
+    int y,m;
+    cout<<"Enter second number and value for taking modulus"<<endl;
+    cin>>y>>m;
+    int z = (x * y) % m;
+    cout<<"Output is :"<<z;
+    return 0;
+}
