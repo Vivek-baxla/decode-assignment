1dc506d167d29e561d718e79efd2f0fd6a975d49
Vivek-baxla <144560722+Vivek-baxla@users.noreply.github.com>
2023-09-22 18:21:09 +0530

    solved assignment are

---
+++ volumeOfCylinder.cpp
@@ -0,0 +1,13 @@
+# include<iostream>
+using namespace std;
+
+int main() {
+    int r, h;
+    cout<<"Enter radius value : "<<endl;
+    cin>>r;
+    cout<<"Enter height value : "<<endl;
+    cin>>h;
+    int volume = 3.14*r*r*h;
+    cout<<"volume of a cylinder : "<<volume;
+    return 0;
+    }
